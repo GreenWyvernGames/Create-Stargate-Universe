e4ea2bf48941b6c023536d1f9a63512b6c418441
n-galrion <nevyn202@outlook.com>
2025-03-19 00:08:58 -0500

    fix typo

--- src/main/java/com/greenwyvern/createstargateverse/CreateStargateUniverse.java
+++ src/main/java/com/greenwyvern/createstargateverse/CreateStargateUniverse.java
@@ -33,8 +33,8 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(CreateStargateUnivers.MODID)
-public class CreateStargateUnivers {
+@Mod(CreateStargateUniverse.MODID)
+public class CreateStargateUniverse {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "csgmod";
     // Directly reference a slf4j logger
@@ -61,7 +61,7 @@ public class CreateStargateUnivers {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public CreateStargateUnivers(IEventBus modEventBus, ModContainer modContainer) {
+    public CreateStargateUniverse(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
